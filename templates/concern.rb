d9dbefc955a09bd8d229127fc9e9748a738b642f
Robert Audi <robert.audii@gmail.com>
2016-06-28 11:31:28 +0200

    Clean, update, patch, etc.

--- templates/concern.rb
+++ templates/concern.rb
@@ -2,16 +2,12 @@ module <%= class_name %>
   extend ActiveSupport::Concern
 <% if options[:extended] -%>
 
-  included do
+  included do |klass|
     # stuff to do at inclusion
   end
 
-  module ClassMethods
-
-  end
-
-  module InstanceMethods
-    
+  class_methods do
+    # ...
   end
 <% end -%>
 end
